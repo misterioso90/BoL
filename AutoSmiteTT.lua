98a7507942523d7fc001e46aa4c274abe97cfa28
BotHappy <Rigle00@gmail.com>
2013-10-31 21:36:15 +0100

    0.3a Fixes

--- AutoSmiteTT.lua
+++ AutoSmiteTT.lua
@@ -6,12 +6,12 @@ Object TT Info: http://pastebin.com/zhDfUzPg
 0.2 - Nunu Q added
 0.2a - Fixed some damages
 0.2b - Fixed Draws
-0.3 - Added NoSmite Nunu]]
+0.3 - Added NoSmite Nunu
+0.3a - Improved Nunu]]
 
 --[[TO-DO
 
-- Fix NoSmite mode bugs
-- Add Trundle Smite with low health (Passive)
+- Add Trundle Smite with low health (Passive) (ON WORK)
 - Add Chogath R
 
 ]]
@@ -25,7 +25,6 @@ local range = 750       -- Range of smite 625+model. Usually 750.
 local turnoff = false   --true/false
 
 local SmiteSlot = nil
-local objminionTable = {}
 local SmiteDamage, qDamage, MixDamage = 0, 0, 0
 local CanUseSmite, CanUseQ = false, false
 local SmiteIsOn = false
@@ -34,42 +33,58 @@ local Vilemaw
 local WolfL, GolemL, WraighL -- Western Camps
 local WolfR, GolemR, WraighR -- Eastern Camps
 
+
+--local BMinion, CMinion, WMinion, MMinion
+
 -- Code
 function CheckSmite()
 	if myHero:GetSpellData(SUMMONER_1).name:find("Smite") then SmiteSlot = SUMMONER_1
 		elseif myHero:GetSpellData(SUMMONER_2).name:find("Smite") then SmiteSlot = SUMMONER_2 end
 end
 
-function OnLoad()
+function Menu()
+    SmiteTT = scriptConfig("AutoSmite TT 0.3a", "autosmiteTT")
+    SmiteTT:addParam("switcher", "Switcher Hotkey", SCRIPT_PARAM_ONKEYTOGGLE, (SmiteSlot ~= nil), 78)
+    SmiteTT:addParam("hold", "Hold Hotkey (CTRL)", SCRIPT_PARAM_ONKEYDOWN, false, 17)
+    SmiteTT:addParam("active", "AutoSmite", SCRIPT_PARAM_INFO, false)
+	SmiteTT:permaShow("active")
+	
+	if myHero.charName == "Nunu" then
+		SmiteTT:addParam("nosmite", "NUNU: Desactivate Smite", SCRIPT_PARAM_ONOFF, false)
+		SmiteTT:addParam("nomix", "NUNU: Desactivate Mixed Damage", SCRIPT_PARAM_ONOFF, false)
+		SmiteTT:permaShow("nosmite")
+		SmiteTT:permaShow("nomix")
+	end
+	
+--[[	if myHero.charName == "Trundle" then
+		SmiteTT:addParam("lasthope", "TRUNDLE: Last Hope Smite", SCRIPT_PARAM_ONOFF, false)
+		SmiteTT:permaShow("lasthope")
+	end]]
+	
+	SmiteTT:addParam("vilemaw", "AutoSmite Vilemaw", SCRIPT_PARAM_ONOFF, true)
+    SmiteTT:addParam("drawrange", "Draw Smite Range", SCRIPT_PARAM_ONOFF, true)
+    SmiteTT:addParam("drawcircles", "Draw Circles", SCRIPT_PARAM_ONOFF, true)
+    SmiteTT:addParam("drawtext", "Draw Text", SCRIPT_PARAM_ONOFF, true)
+end
 
+function OnLoad()
     CheckSmite()
 	if myHero.charName == "Nunu" or SmiteSlot or not turnoff then
-                SmiteTT = scriptConfig("AutoSmite TT 0.3", "autosmiteTT")
-                SmiteTT:addParam("switcher", "Switcher Hotkey", SCRIPT_PARAM_ONKEYTOGGLE, (SmiteSlot ~= nil), 78)
-                SmiteTT:addParam("hold", "Hold Hotkey (CTRL)", SCRIPT_PARAM_ONKEYDOWN, false, 17)
-                SmiteTT:addParam("active", "AutoSmite", SCRIPT_PARAM_INFO, false)
-				SmiteTT:permaShow("active")
-				
-				if myHero.charName == "Nunu" then
-				SmiteTT:addParam("nosmite", "Desactivate Smite", SCRIPT_PARAM_ONOFF, false)
-				SmiteTT:permaShow("nosmite")
-				end
-				
-				SmiteTT:addParam("vilemaw", "AutoSmite Vilemaw", SCRIPT_PARAM_ONOFF, true)
-                SmiteTT:addParam("drawrange", "Draw Smite Range", SCRIPT_PARAM_ONOFF, true)
-                SmiteTT:addParam("drawcircles", "Draw Circles", SCRIPT_PARAM_ONOFF, true)
-                SmiteTT:addParam("drawtext", "Draw Text", SCRIPT_PARAM_ONOFF, true)
-               
-                ASLoadMinions()
-                SmiteIsOn = true
-                PrintChat(" >> AutoSmite TT 0.3 by BotHappy")
-        end
+		Menu()
+        ASLoadMinions()
+        SmiteIsOn = true --Everything going well
+        PrintChat(" >> AutoSmite TT 0.3a by BotHappy")
+    end
+end
+
+function Damages()
+	SmiteDamage = 460+30*myHero.level
+	qDamage = 500+125*(myHero:GetSpellData(_Q).level-1)
+	MixDamage = qDamage+SmiteDamage
 end
 
 function OnTick()
 	if not SmiteIsOn then return end
-	if SmiteTT.nosmite == false then CheckSmite() end
-	if SmiteTT.nosmite == true and Vilemaw == nil then SmiteSlot = nil end
 	
 	CheckDeadMonsters()
 
@@ -77,39 +92,47 @@ function OnTick()
 
     if not SmiteTT.active and SmiteTT.vilemaw and Vilemaw ~= nil then 
 		SmiteTT.active = true
-		SmiteTT.nosmite = false end
+	end
 		
 	CanUseQ = (myHero.charName == "Nunu" and myHero:CanUseSpell(_Q) == READY)
-	
-    SmiteDamage = 460+30*myHero.level
-	qDamage = 500+125*(myHero:GetSpellData(_Q).level-1)
-	MixDamage = qDamage+SmiteDamage
+	Damages()
 
 	if SmiteSlot ~= nil then CanUseSmite = (myHero:CanUseSpell(SmiteSlot) == READY) end
   
-    if SmiteTT.active and not myHero.dead and (CanUseSmite or CanUseQ) then
-        if Vilemaw ~= nil then CheckMonster(Vilemaw) end
-        if GolemL ~= nil then CheckMonster(GolemL) end
-        if WolfL ~= nil then CheckMonster(WolfL) end
-        if WraighL ~= nil then CheckMonster(WraighL) end
-		if GolemR ~= nil then CheckMonster(GolemR) end
-        if WolfR ~= nil then CheckMonster(WolfR) end
-        if WraighR ~= nil then CheckMonster(WraighR) end
-    end
+	if SmiteTT.active and not myHero.dead and (CanUseSmite or CanUseQ) then
+		if Vilemaw ~= nil then TryToKillObjetive(Vilemaw) end
+		if GolemL ~= nil then TryToKillObjetive(GolemL) end
+		if WolfL ~= nil then TryToKillObjetive(WolfL) end
+		if WraighL ~= nil then TryToKillObjetive(WraighL) end
+		if GolemR ~= nil then TryToKillObjetive(GolemR) end
+		if WolfR ~= nil then TryToKillObjetive(WolfR) end
+		if WraighR ~= nil then TryToKillObjetive(WraighR) end
+	end
+	
+--[[	if myHero.charName == "Trundle" then
+		LifeSaver()
+	
+		if SmiteTT.active and not myHero.dead and SmiteTT.lasthope then
+			if BMinion ~= nil then if CanUseSmite and BMinion.health <= SmiteDamage then CastSpell(SmiteSlot, object) end end
+			if CMinion ~= nil then if CanUseSmite and CMinion.health <= SmiteDamage then CastSpell(SmiteSlot, object) end end
+			if WMinion ~= nil then if CanUseSmite and WMinion.health <= SmiteDamage then CastSpell(SmiteSlot, object) end end
+			if MMinion ~= nil then if CanUseSmite and MMinion.health <= SmiteDamage then CastSpell(SmiteSlot, object) end end
+		end
+	end]]
 end
 
-function CheckMonster(object)
+function TryToKillObjetive(object)
     if object ~= nil and not object.dead and object.visible and object.x ~= nil then
         local DistanceMonster = GetDistance(object)
-        if CanUseSmite and DistanceMonster <= range and object.health <= SmiteDamage then
-			CastSpell(SmiteSlot, object)
-		elseif CanUseQ and DistanceMonster <=125+200 then
+        if CanUseQ and DistanceMonster <=125+200 then
 			if object.health <= qDamage then
-					CastSpell(_Q, object)
-			elseif CanUseSmite and object.health <= MixDamage then
-				if DistanceMonster <=125+200 then
-				CastSpell(_Q, object)
+			CastSpell(_Q, object)
+			elseif CanUseSmite and not SmiteTT.nosmite and DistanceMonster <= range and object.health <= SmiteDamage then
 				CastSpell(SmiteSlot, object)
+				elseif not SmiteTT.nomix and CanUseSmite and object.health <= MixDamage then
+				if DistanceMonster <=125+200 then
+					CastSpell(_Q, object)
+					CastSpell(SmiteSlot, object)
 				end
 			end
         end
@@ -140,11 +163,11 @@ function MonsterDraw(object)
         if SmiteTT.active and SmiteTT.drawcircles and CanUseSmite and DistanceMonster <= range then
             local healthradius = object.health*100/object.maxHealth
             DrawCircle(object.x, object.y, object.z, healthradius+100, 0x00FF00)
-            if CanUseSmite then
+            if CanUseSmite and not SmiteTT.nosmite then
                 local smitehealthradius = SmiteDamage*100/object.maxHealth
                 DrawCircle(object.x, object.y, object.z, smitehealthradius+100, 0x00FFFF)
             end
-			if CanUseQ and CanUseSmite then
+			if CanUseQ and CanUseSmite and not Smite.nomix then
                 local Qsmitehealthradius = MixDamage*100/object.maxHealth
                 DrawCircle(object.x, object.y, object.z, Qsmitehealthradius+100, 0x00FFFF)
 			end
@@ -187,12 +210,13 @@ function OnCreateObj(obj)
     if not SmiteIsOn then return end
     if obj ~= nil and obj.type == "obj_AI_Minion" and obj.name ~= nil then
         if obj.name == "TT_Spiderboss8.1.1" then Vilemaw = obj
-        elseif obj.name == "TT_NWolf3.1.1" then WolfL = obj
-        elseif obj.name == "TT_NWraith1.1.1" then WraighL = obj
-        elseif obj.name == "TT_NGolem2.1.1" then GolemL = obj  
-		elseif obj.name == "TT_NWolf6.1.1" then WolfR = obj
-        elseif obj.name == "TT_NWraith4.1.1" then WraighR = obj
-        elseif obj.name == "TT_NGolem5.1.1" then GolemR = obj end
+			elseif obj.name == "TT_NWolf3.1.1" then WolfL = obj
+			elseif obj.name == "TT_NWraith1.1.1" then WraighL = obj
+			elseif obj.name == "TT_NGolem2.1.1" then GolemL = obj  
+			elseif obj.name == "TT_NWolf6.1.1" then WolfR = obj
+			elseif obj.name == "TT_NWraith4.1.1" then WraighR = obj
+			elseif obj.name == "TT_NGolem5.1.1" then GolemR = obj
+		end
     end
 end
 
@@ -200,12 +224,13 @@ function OnDeleteObj(obj)
     if not SmiteIsOn then return end
     if obj ~= nil and obj.name ~= nil then
         if obj.name == "TT_Spiderboss8.1.1" then Vilemaw = nil
-        elseif obj.name == "TT_NWolf3.1.1" then WolfL = nil
-        elseif obj.name == "TT_NWraith1.1.1" then WraighL = nil
-        elseif obj.name == "TT_NGolem2.1.1" then GolemL = nil
-		elseif obj.name == "TT_NWolf6.1.1" then WolfR = nil
-        elseif obj.name == "TT_NWraith4.1.1" then WraighR = nil
-        elseif obj.name == "TT_NGolem5.1.1" then GolemR = nil 				end
+			elseif obj.name == "TT_NWolf3.1.1" then WolfL = nil
+			elseif obj.name == "TT_NWraith1.1.1" then WraighL = nil
+			elseif obj.name == "TT_NGolem2.1.1" then GolemL = nil
+			elseif obj.name == "TT_NWolf6.1.1" then WolfR = nil
+			elseif obj.name == "TT_NWraith4.1.1" then WraighR = nil
+			elseif obj.name == "TT_NGolem5.1.1" then GolemR = nil
+		end
     end
 end
 
@@ -224,13 +249,31 @@ function ASLoadMinions()
         local obj = objManager:getObject(i)
         if obj ~= nil and obj.type == "obj_AI_Minion" and obj.name ~= nil then
             if obj.name == "TT_Spiderboss8.1.1" then Vilemaw = obj
-            elseif obj.name == "TT_NWolf3.1.1" then WolfL = obj
-            elseif obj.name == "TT_NWraith1.1.1" then WraighL = obj
-            elseif obj.name == "TT_NGolem2.1.1" then GolemL = obj
-			elseif obj.name == "TT_NWolf6.1.1" then WolfR = obj
-            elseif obj.name == "TT_NWraith4.1.1" then WraighR = obj
-            elseif obj.name == "TT_NGolem5.1.1" then GolemR = obj
+				elseif obj.name == "TT_NWolf3.1.1" then WolfL = obj
+				elseif obj.name == "TT_NWraith1.1.1" then WraighL = obj
+				elseif obj.name == "TT_NGolem2.1.1" then GolemL = obj
+				elseif obj.name == "TT_NWolf6.1.1" then WolfR = obj
+				elseif obj.name == "TT_NWraith4.1.1" then WraighR = obj
+				elseif obj.name == "TT_NGolem5.1.1" then GolemR = obj
+--				elseif obj.name == "_Minion_Basic" then BMinion = obj
+--				elseif obj.name == "_Minion_Caster" then CMinion = obj
+--				elseif obj.name == "_Minion_Wizard" then WMinion = obj
+--				elseif obj.name == "_Minion_MechCannon" then MMinion = obj
 			end
 		end
     end
 end
+
+--[[function LifeSaver()
+	for i = 1, objManager.maxObjects do
+		local obj = objManager:getObject(i)
+		if obj ~= nil and obj.type == "obj_AI_Minion" and obj.name ~= nil then
+			if obj.name == "_Minion_Basic" then BMinion = obj
+				elseif obj.name == "_Minion_Caster" then CMinion = obj
+				elseif obj.name == "_Minion_Wizard" then WMinion = obj
+				elseif obj.name == "_Minion_MechCannon" then MMinion = obj
+			end
+		end
+	end
+	
+end]]
