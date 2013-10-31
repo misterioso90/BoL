dc7166ab47d9562ce5a1ec5ab4265fee6d05164f
BotHappy <Rigle00@gmail.com>
2013-10-31 19:54:33 +0100

    Update with prodiction

--- SidasAutoCarryPlugin - Shyvana.lua
+++ SidasAutoCarryPlugin - Shyvana.lua
@@ -1,19 +1,30 @@
 --[[ Shyvana Prodiction by BotHappy
 
 0.1 - Prodiction Adapted to Shyvana
-0.2 - AutoCarry Script w/o Prodiction]]
+0.2 - AutoCarry Script w/o Prodiction
+0.3 - Prodiction Update]]
 
 require "Collision"
+require "Prodiction"
 
 if myHero.charName ~= "Shyvana" then return end
+
+if not VIP_USER then
+	print("Shyvana Tiem is VIP Only ATM.")
+	return
+end
 
 local QRange = 125
 local WRange = 325
 local ERange = 925
 local RRange = 1000
 local QAble, WAble, EAble, RAble = false, false, false, false
-local SkillE = {spellKey = _E, range = ERange, speed = 1.5, delay = 125, width = 80}
-local SkillR = {spellKey = _R, range = RRange, speed = 1.0, delay = 250, width = 80}
+
+local Prodict = ProdictManager.GetInstance()
+local ProdictE, ProdictR
+local ts = {}
+--local SkillE = {spellKey = _E, range = ERange, speed = 1.5, delay = 125, width = 80}
+--local SkillR = {spellKey = _R, range = RRange, speed = 1.0, delay = 250, width = 80}
 
 function Menu()
 	AutoCarry.PluginMenu:addParam("useQ", "Use Q in combo", SCRIPT_PARAM_ONOFF, true)
@@ -25,10 +36,43 @@ function Menu()
 	AutoCarry.PluginMenu:addParam("drawR", "Draw R Range", SCRIPT_PARAM_ONOFF, true)
 end
 
+local function getHitBoxRadius(target)
+	return GetDistance(target, target.minBBox)
+end
+
+function CastE(unit, pos, spell)
+	if GetDistance(pos) - getHitBoxRadius(unit)/2 < ERange then
+		CastSpell(_E, pos.x, pos.z) 
+	end
+end
+
+function CastR(unit, pos, spell)
+	if GetDistance(pos) - getHitBoxRadius(unit)/2 < RRange then
+		CastSpell(_R, pos.x, pos.z) 
+	end
+end
+
 function PluginOnLoad()
 	AutoCarry.SkillsCrosshair.range = 1100
 	Menu()
-	PrintChat(">>Shyvana 0.1 Loaded")
+	
+	ProdictE = Prodict:AddProdictionObject(_E, 925, 1500, 0.125, 80, myHero, CastE) --925+125? myHero:GetSpellData(_E).lineWidth
+	for I = 1, heroManager.iCount do
+		local hero = heroManager:GetHero(I)
+		if hero.team ~= myHero.team then
+			ProdictE:CanNotMissMode(true, hero)
+		end
+	end
+	
+	ProdictR = Prodict:AddProdictionObject(_R, 1000, 1000, 0.125, 80, myHero, CastR) -- myHero:GetSpellData(_R).lineWidth
+	for I = 1, heroManager.iCount do
+		local hero = heroManager:GetHero(I)
+		if hero.team ~= myHero.team then
+			ProdictR:CanNotMissMode(true, hero)
+		end
+	end
+	
+	PrintChat(">> Shyvana Tiem 0.3 Loaded")
 end
 
 function PluginOnTick()
@@ -47,8 +91,8 @@ function ComboCast()
 		end
 	end
 	if WAble and AutoCarry.PluginMenu.useW then CastSpell(_W, Target) end
-	if EAble and AutoCarry.PluginMenu.useE then AutoCarry.CastSkillshot(SkillE, Target) end
-	if RAble and AutoCarry.PluginMenu.useR then AutoCarry.CastSkillshot(SkillR, Target) end
+	if EAble and AutoCarry.PluginMenu.useE then ProdictE:EnableTarget(Target, true) end
+	if RAble and AutoCarry.PluginMenu.useR then ProdictR:EnableTarget(Target, true) end
 end
 
 function Checks()
