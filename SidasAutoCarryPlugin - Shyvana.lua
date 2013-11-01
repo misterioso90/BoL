9f6fcbbefc24b7f3ba51d5c446994d00c9cea88a
BotHappy <Rigle00@gmail.com>
2013-11-01 02:21:58 +0100

    0.4 Added simple item usage

--- SidasAutoCarryPlugin - Shyvana.lua
+++ SidasAutoCarryPlugin - Shyvana.lua
@@ -1,10 +1,11 @@
---[[ Shyvana Prodiction by BotHappy
+--[[ Shyvana Prodiction Autocarry Plugin by BotHappy
 
 0.1 - Prodiction Adapted to Shyvana
 0.2 - AutoCarry Script w/o Prodiction
-0.3 - Prodiction Update]]
+0.3 - Prodiction Update
+0.4 - Added Item Usage]]
 
-require "Collision"
+--require "Collision"
 require "Prodiction"
 
 if myHero.charName ~= "Shyvana" then return end
@@ -22,9 +23,79 @@ local QAble, WAble, EAble, RAble = false, false, false, false
 
 local Prodict = ProdictManager.GetInstance()
 local ProdictE, ProdictR
-local ts = {}
---local SkillE = {spellKey = _E, range = ERange, speed = 1.5, delay = 125, width = 80}
---local SkillR = {spellKey = _R, range = RRange, speed = 1.0, delay = 250, width = 80}
+
+local items =
+		{
+				BRK = {id=3153, range = 500, reqTarget = true, slot = nil },
+                BWC = {id=3144, range = 400, reqTarget = true, slot = nil },
+				DFG = {id=3128, range = 750, reqTarget = true, slot = nil },
+                HGB = {id=3146, range = 400, reqTarget = true, slot = nil },
+                RSH = {id=3074, range = 350, reqTarget = false, slot = nil},
+                STD = {id=3131, range = 350, reqTarget = false, slot = nil},
+                TMT = {id=3077, range = 350, reqTarget = false, slot = nil},
+                YGB = {id=3142, range = 350, reqTarget = false, slot = nil}
+		}
+
+		
+function PluginOnLoad()
+	AutoCarry.SkillsCrosshair.range = 1100
+	Menu()
+	
+	ProdictE = Prodict:AddProdictionObject(_E, 925, 1500, 0.125, 80, myHero, CastE) --925+125? myHero:GetSpellData(_E).lineWidth
+	ProdictR = Prodict:AddProdictionObject(_R, 1000, 1000, 0.125, 80, myHero, CastR) -- myHero:GetSpellData(_R).lineWidth
+	for I = 1, heroManager.iCount do
+		local hero = heroManager:GetHero(I)
+		if hero.team ~= myHero.team then
+			ProdictE:CanNotMissMode(true, hero)
+			ProdictR:CanNotMissMode(true, hero)
+		end
+	end
+	
+	PrintChat(">> Shyvana Tiem 0.4 Loaded")
+end
+
+function PluginOnTick()
+	Checks()
+	if Target then
+		if (AutoCarry.MainMenu.AutoCarry or AutoCarry.MainMenu.MixedMode) then
+			ComboCast()
+		end
+	end
+end
+
+function PluginOnDraw()
+	if not myHero.dead then
+		if WAble and AutoCarry.PluginMenu.drawW then
+			DrawCircle(myHero.x, myHero.y, myHero.z, WRange, 0x7CFC00)
+		end
+		if EAble and AutoCarry.PluginMenu.drawE then 
+			DrawCircle(myHero.x, myHero.y, myHero.z, ERange, 0x00FFFF)
+		end
+		if RAble and AutoCarry.PluginMenu.drawR then
+			DrawCircle(myHero.x, myHero.y, myHero.z, RRange, 0xFF0000)
+		end
+	end
+end
+
+function ComboCast() 
+	UseItems(Target)
+	if QAble and AutoCarry.PluginMenu.useQ then
+		if GetDistance(Target) <= QRange  and AutoCarry.shotFired then
+			CastSpell(_Q, Target)
+		end
+	end
+	if WAble and AutoCarry.PluginMenu.useW then CastSpell(_W, Target) end
+	if EAble and AutoCarry.PluginMenu.useE then ProdictE:EnableTarget(Target, true) end
+	if RAble and AutoCarry.PluginMenu.useR then ProdictR:EnableTarget(Target, true) end
+end
+
+function Checks()
+	QAble = (myHero:CanUseSpell(_Q) == READY)
+	WAble = (myHero:CanUseSpell(_W) == READY)
+	EAble = (myHero:CanUseSpell(_E) == READY)
+	RAble = (myHero:CanUseSpell(_R) == READY)
+	Target = AutoCarry.GetAttackTarget()
+end
 
 function Menu()
 	AutoCarry.PluginMenu:addParam("useQ", "Use Q in combo", SCRIPT_PARAM_ONOFF, true)
@@ -52,67 +123,19 @@ function CastR(unit, pos, spell)
 	end
 end
 
-function PluginOnLoad()
-	AutoCarry.SkillsCrosshair.range = 1100
-	Menu()
-	
-	ProdictE = Prodict:AddProdictionObject(_E, 925, 1500, 0.125, 80, myHero, CastE) --925+125? myHero:GetSpellData(_E).lineWidth
-	for I = 1, heroManager.iCount do
-		local hero = heroManager:GetHero(I)
-		if hero.team ~= myHero.team then
-			ProdictE:CanNotMissMode(true, hero)
-		end
-	end
-	
-	ProdictR = Prodict:AddProdictionObject(_R, 1000, 1000, 0.125, 80, myHero, CastR) -- myHero:GetSpellData(_R).lineWidth
-	for I = 1, heroManager.iCount do
-		local hero = heroManager:GetHero(I)
-		if hero.team ~= myHero.team then
-			ProdictR:CanNotMissMode(true, hero)
-		end
-	end
-	
-	PrintChat(">> Shyvana Tiem 0.3 Loaded")
-end
-
-function PluginOnTick()
-	Checks()
-	if Target then
-		if (AutoCarry.MainMenu.AutoCarry or AutoCarry.MainMenu.MixedMode) then
-			ComboCast()
-		end
-	end
-end
-
-function ComboCast() 
-	if QAble and AutoCarry.PluginMenu.useQ then
-		if GetDistance(Target) <= QRange  and AutoCarry.shotFired then
-			CastSpell(_Q, Target)
-		end
-	end
-	if WAble and AutoCarry.PluginMenu.useW then CastSpell(_W, Target) end
-	if EAble and AutoCarry.PluginMenu.useE then ProdictE:EnableTarget(Target, true) end
-	if RAble and AutoCarry.PluginMenu.useR then ProdictR:EnableTarget(Target, true) end
-end
-
-function Checks()
-	QAble = (myHero:CanUseSpell(_Q) == READY)
-	WAble = (myHero:CanUseSpell(_W) == READY)
-	EAble = (myHero:CanUseSpell(_E) == READY)
-	RAble = (myHero:CanUseSpell(_R) == READY)
-	Target = AutoCarry.GetAttackTarget()
-end
-
-function PluginOnDraw()
-	if not myHero.dead then
-		if WAble and AutoCarry.PluginMenu.drawW then
-			DrawCircle(myHero.x, myHero.y, myHero.z, WRange, 0x7CFC00)
-		end
-		if EAble and AutoCarry.PluginMenu.drawE then 
-			DrawCircle(myHero.x, myHero.y, myHero.z, ERange, 0x00FFFF)
-		end
-		if RAble and AutoCarry.PluginMenu.drawR then
-			DrawCircle(myHero.x, myHero.y, myHero.z, RRange, 0xFF0000)
-		end
-	end
+-- By Sida
+function UseItems(target)
+        if target == nil then return end
+        for _,item in pairs(items) do
+            item.slot = GetInventorySlotItem(item.id)
+            if item.slot ~= nil then
+                if item.reqTarget and GetDistance(target) < item.range then
+                    CastSpell(item.slot, target)
+                    elseif not item.reqTarget then
+                        if (GetDistance(target) - getHitBoxRadius(myHero) - getHitBoxRadius(target)) < 50 then
+                            CastSpell(item.slot)
+                        end
+                end
+            end
+        end
 end
