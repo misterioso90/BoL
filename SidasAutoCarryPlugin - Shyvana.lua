c0b7fa8e36d04a4139f5e9fa0eaa688a6f16dc7c
BotHappy <Rigle00@gmail.com>
2013-11-03 00:47:28 +0100

    Harrass + Dragon + Fixes

--- SidasAutoCarryPlugin - Shyvana.lua
+++ SidasAutoCarryPlugin - Shyvana.lua
@@ -3,7 +3,8 @@
 0.1 - Prodiction Adapted to Shyvana
 0.2 - AutoCarry Script w/o Prodiction
 0.3 - Prodiction Update
-0.4 - Added Item Usage]]
+0.4 - Added Item Usage
+0.5 - Dragon + Fixes + Harrass]]
 
 --require "Collision"
 require "Prodiction"
@@ -19,7 +20,7 @@ local QRange = 125
 local WRange = 325
 local ERange = 925
 local RRange = 1000
-local QAble, WAble, EAble, RAble = false, false, false, false
+local QAble, WAble, EAble, RAble, WActive, DragonActive = false, false, false, false, false, false
 
 local Prodict = ProdictManager.GetInstance()
 local ProdictE, ProdictR
@@ -51,7 +52,7 @@ function PluginOnLoad()
 		end
 	end
 	
-	PrintChat(">> Shyvana Tiem 0.4 Loaded")
+	PrintChat(">> Shyvana Tiem 0.5 Loaded")
 end
 
 function PluginOnTick()
@@ -60,12 +61,15 @@ function PluginOnTick()
 		if (AutoCarry.MainMenu.AutoCarry or AutoCarry.MainMenu.MixedMode) then
 			ComboCast()
 		end
+		if AutoCarry.PluginMenu.Harrass and EAble then
+			ProdictE:EnableTarget(Target, true)
+		end
 	end
 end
 
 function PluginOnDraw()
 	if not myHero.dead then
-		if WAble and AutoCarry.PluginMenu.drawW then
+		if (WAble or WActive) and AutoCarry.PluginMenu.drawW then
 			DrawCircle(myHero.x, myHero.y, myHero.z, WRange, 0x7CFC00)
 		end
 		if EAble and AutoCarry.PluginMenu.drawE then 
@@ -81,10 +85,10 @@ function ComboCast()
 	UseItems(Target)
 	if QAble and AutoCarry.PluginMenu.useQ then
 		if GetDistance(Target) <= QRange  and AutoCarry.shotFired then
-			CastSpell(_Q, Target)
+			CastSpell(_Q)
 		end
 	end
-	if WAble and AutoCarry.PluginMenu.useW then CastSpell(_W, Target) end
+	if WAble and AutoCarry.PluginMenu.useW then CastSpell(_W) end
 	if EAble and AutoCarry.PluginMenu.useE then ProdictE:EnableTarget(Target, true) end
 	if RAble and AutoCarry.PluginMenu.useR then ProdictR:EnableTarget(Target, true) end
 end
@@ -95,9 +99,15 @@ function Checks()
 	EAble = (myHero:CanUseSpell(_E) == READY)
 	RAble = (myHero:CanUseSpell(_R) == READY)
 	Target = AutoCarry.GetAttackTarget()
+	if DragonActive then
+		ERange = 700
+	elseif not DragonActive then
+		ERange = 925
+	end
 end
 
 function Menu()
+	local HKE = string.byte("T")
 	AutoCarry.PluginMenu:addParam("useQ", "Use Q in combo", SCRIPT_PARAM_ONOFF, true)
 	AutoCarry.PluginMenu:addParam("useW", "Use W in combo", SCRIPT_PARAM_ONOFF, true)
 	AutoCarry.PluginMenu:addParam("useE", "Use E in combo", SCRIPT_PARAM_ONOFF, true)
@@ -105,6 +115,8 @@ function Menu()
 	AutoCarry.PluginMenu:addParam("drawW", "Draw W Range", SCRIPT_PARAM_ONOFF, true)
 	AutoCarry.PluginMenu:addParam("drawE", "Draw E Range", SCRIPT_PARAM_ONOFF, true)
 	AutoCarry.PluginMenu:addParam("drawR", "Draw R Range", SCRIPT_PARAM_ONOFF, true)
+	AutoCarry.PluginMenu:addParam("Harrass", "Harrass with E", SCRIPT_PARAM_ONKEYDOWN, false, HKE)
+	AutoCarry.PluginMenu:permaShow("Harrass")
 end
 
 local function getHitBoxRadius(target)
@@ -125,17 +137,35 @@ end
 
 -- By Sida
 function UseItems(target)
-        if target == nil then return end
-        for _,item in pairs(items) do
-            item.slot = GetInventorySlotItem(item.id)
-            if item.slot ~= nil then
-                if item.reqTarget and GetDistance(target) < item.range then
-                    CastSpell(item.slot, target)
-                    elseif not item.reqTarget then
-                        if (GetDistance(target) - getHitBoxRadius(myHero) - getHitBoxRadius(target)) < 50 then
-                            CastSpell(item.slot)
-                        end
+    if target == nil then return end
+    for _,item in pairs(items) do
+        item.slot = GetInventorySlotItem(item.id)
+        if item.slot ~= nil then
+            if item.reqTarget and GetDistance(target) < item.range then
+                CastSpell(item.slot, target)
+                elseif not item.reqTarget then
+                if (GetDistance(target) - getHitBoxRadius(myHero) - getHitBoxRadius(target)) < 50 then
+                    CastSpell(item.slot)
                 end
             end
         end
+    end
+end
+
+function OnGainBuff(myHero, buff)
+	if buff.name == "ShyvanaImmolationAura" then
+		WActive = true
+	end
+	if buff.name == "shyvanatransform" then
+		DragonActive = true
+	end
+end
+
+function OnLoseBuff(myHero, buff)
+	if buff.name == "ShyvanaImmolationAura" then
+		WActive = false
+	end
+	if buff.name == "shyvanatransform" then
+		DragonActive = false
+	end
 end
